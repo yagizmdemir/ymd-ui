92ad6ae99d79ad79f49808dace43ff9f146fcaca
yagizmdemir <yagizmdemir@gmail.com>
2023-08-27 01:38:50 +0300

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'ymd-ui'
+import 'ymd-ui/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
